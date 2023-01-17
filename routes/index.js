93d722fb16c91031b7c6c2fe6522dd18eec869b3
Choskas Delta <Choskas Delta>
2023-01-16 23:13:12 -0600

    fix: elete api

--- routes/index.js
+++ routes/index.js
@@ -58,8 +58,8 @@ router.put("/api/edit-theme", async (req, res, next) => {
 
 router.delete("/api/delete-theme", async (req, res, next) => {
   try {
-    errorNoID(req.params.id, res);
-    await Themes.findByIdAndDelete(req.params.id);
+    errorNoID(req.query.id, res);
+    await Themes.findByIdAndDelete(req.query.id);
     return res
       .status(200)
       .json({ message: "Tema borrado exitosamente", error: false });
